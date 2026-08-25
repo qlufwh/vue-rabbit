2a6f489bffa3d7c6fe767f118899375f74eae765
fanwenhui <QlFwh@outlook.com>
2026-08-26 03:07:03 +0800

    配置AXios,路由

---
+++ src/views/Category/index.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        分类
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        HOMO
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
