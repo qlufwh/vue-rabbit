aee63acdd0c46167df03e9712c2edbcc29805f77
fanwenhui <QlFwh@outlook.com>
2026-08-27 17:52:55 +0800

    整体结构拆分,以及分类数据

---
+++ src/views/Home/components/HomeBanner.vue
@@ -0,0 +1,3 @@
+<template>
+ban
+</template>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,13 +1,17 @@
-<template lang="">
-    <div>
-        HOMO
-    </div>
-</template>
-<script>
-export default {
-    
-}
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
 </script>
-<style lang="">
-    
-</style>
+
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
+</template>
