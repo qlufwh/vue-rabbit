9c8828f956f47130783cf1559b9d46b9716b1399
fanwenhui <QlFwh@outlook.com>
2026-08-30 03:50:42 +0800

    列表筛选功能初实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -44,6 +44,12 @@ onMounted(() => {
     getGoodList()
     getCategoryData()
 })
+// tab切换回调
+const tabChange = () => {
+    console.log('tab切换了', reqData.value.sortField)
+    reqData.value.page = 1
+    getGoodList()
+}
 </script>
 
 <template>
@@ -53,22 +59,23 @@ onMounted(() => {
             <el-breadcrumb separator=">">
                 <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
                 <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{ categoryData.parentName
-                }}</el-breadcrumb-item>
+                    }}</el-breadcrumb-item>
                 <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
         <div class="sub-container">
-            <el-tabs>
+            <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
                 <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
             <div class="body">
                 <!-- 商品列表-->
-                 <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
+                <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
             </div>
         </div>
     </div>
+
 </template>
 
 <style lang="scss" scoped>
