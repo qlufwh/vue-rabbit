cf7018a52b79a9d76b5c0ac095e7aa672ba56473
fanwenhui <QlFwh@outlook.com>
2026-09-10 15:19:16 +0800

    本地购物车单选状态

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -29,7 +29,12 @@ export const useCartStore = defineStore('cart', () => {
     const idx = cartList.value.findIndex((item) => item.skuId === skuId)
     cartList.value.splice(idx, 1)
   }
-  
+  // 单选功能
+  const singleCheck = (skuId, selected) => {
+    // 通过skuId找到要修改的那一项 然后把它的selected修改为传过来的selected
+    const item = cartList.value.find((item) => item.skuId === skuId)
+    item.selected = selected
+  }
   // 计算属性 - 总数量
   const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
   // 计算属性 - 总价
@@ -38,6 +43,7 @@ export const useCartStore = defineStore('cart', () => {
     cartList,
     addCart,
     delCart,
+    singleCheck,
     allCount,
     allPrice
   }
