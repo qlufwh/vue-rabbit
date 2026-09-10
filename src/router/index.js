19ffdfae5acd89357d89ddadea124dae1f359ade
fanwenhui <QlFwh@outlook.com>
2026-09-10 15:13:30 +0800

    头部购物车计算

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Login from '@/views/Login/index.vue'
 import Detail from "@/views/Detail/index.vue"
+import CartList from '@/views/CartList/index.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -37,6 +38,10 @@ const router = createRouter({
         {
           path: 'detail/:id',
           component: Detail
+        },
+        {
+          path: 'cartlist',
+          component: CartList
         }
       ]
     },
